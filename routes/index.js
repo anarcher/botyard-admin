a159a6c9f9ee0ba160479b4a5d4fa420cdef439c
Outsider <outsideris@gmail.com>
2015-08-10 21:46:41 +0900

    add a sign-in page

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index');
+});
+
+router.post('/signin', function(req, res, next) {
+  res.send('not implemented yet')
 });
 
 module.exports = router;
